2bd80d1b6ee13db6cce6365bcd8e9bc69a7a21fe
al3464 <42984740+al3464@users.noreply.github.com>
2021-02-06 22:50:45 -0500

    variables and expressions

---
+++ ArcherLai_PE3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcherLai_PE3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val;
+            Console.WriteLine("Input from user: ");
+            val = Console.ReadLine();
+
+            // convert to integer
+            int res = Convert.ToInt32(val);
+
+            // display the line
+            Console.WriteLine("Input = {0}", res);
+
+        }
+    }
+}
