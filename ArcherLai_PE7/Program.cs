94c5412da8e8e15686043f33a3433009de9e38f6
al3464 <42984740+al3464@users.noreply.github.com>
2021-02-13 22:33:58 -0500

    PE7

--- ArcherLai_PE7/Program.cs
+++ ArcherLai_PE7/Program.cs
@@ -6,27 +6,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ArcherLai_PE7
-{   
+{
     // Class Program
     // Author: ArcherLai
     // Purpose: Make a mad libs game
     // Restrictions: follow the instructions
 
-        class Program
+    class Program
     {
-            // Method: Main
-            // Purpose: To understand the string.replace() and split() method
-            //          
-            //          
-            // Restrictions: None
+        // Method: Main
+        // Purpose: To understand the string.replace() and split() method
+        //          
+        //          
+        // Restrictions: None
 
         public static void Main(string[] args)
         {
-            
+
             Console.WriteLine("Do you want to play Mad Libs?");//ask if you want to play this game
-            
+
             string text = File.ReadAllText(@"c:\\templates\\MadLibsTemplate.txt");//read the txt file
-            
+
             while (true)//set while method to do valid testing
             {
                 string yesOrNo = Console.ReadLine();
@@ -35,7 +35,7 @@ namespace ArcherLai_PE7
 
                     Console.WriteLine("Ok lets go!");
                     break;
-                   
+
                 }
                 else if (yesOrNo == "no")
                 {
@@ -51,40 +51,58 @@ namespace ArcherLai_PE7
 
             Console.WriteLine("What's your name?");//inset user's name
             string name = Console.ReadLine();
-            
+
             Console.WriteLine("Okay, " + name + "! Could you choose a number between 1 to 6?");//ask what number they want to choose
-            string resultString = Console.ReadLine();
+
             char[] delimiterChars = { '#' };
             string[] lines = text.Split(delimiterChars);//set split method
-                                 
-                if (resultString == "1")
+
+            for (int counter = 0; counter < 3; counter++)
+            {
                 {
-                char[] Replace = { '{', '}' };
-                Console.WriteLine(lines[0].Replace('{',' ').Replace('}',' ').Replace('_',' '));//replace the characters
+                    string resultString = Console.ReadLine();
+
+                    if (resultString == "1")
+                    {
+                        char[] Replace = { '{', '}' };
+                        Console.WriteLine(lines[0].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));//replace the characters
+                        break;
+                    }
+                    else if (resultString == "2")
+                    {
+                        Console.WriteLine(lines[1].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
+                        break;
+                    }
+                    else if (resultString == "3")
+                    {
+                        Console.WriteLine(lines[2].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
+                        break;
+                    }
+                    else if (resultString == "4")
+                    {
+                        Console.WriteLine(lines[3].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
+                        break;
+                    }
+                    else if (resultString == "5")
+                    {
+                        Console.WriteLine(lines[4].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
+                        break;
+                    }
+                    else if (resultString == "6")
+                    {
+                        Console.WriteLine(lines[5].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Come on, " + name + "Try harder!");
+                        continue;
+                    }
                 }
-                else if (resultString == "2")
-                {
-                    Console.WriteLine(lines[1].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
-                }
-                else if (resultString == "3")
-                {
-                    Console.WriteLine(lines[2].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
-                }
-                else if (resultString == "4")
-                {
-                    Console.WriteLine(lines[3].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
-                }
-                else if (resultString == "5")
-                {
-                    Console.WriteLine(lines[4].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
-                }
-                else if (resultString == "6")
-                {
-                    Console.WriteLine(lines[5].Replace('{', ' ').Replace('}', ' ').Replace('_', ' '));
-                }
-            
 
 
+            }
         }
     }
 }
+
