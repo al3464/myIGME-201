6ab664aa79b533c121224c994f9b8ab5ca61e791
al3464 <42984740+al3464@users.noreply.github.com>
2021-02-17 23:45:45 -0500

    PE9

---
+++ ArcherLai_PE9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Timers;
+
+namespace TimerAppConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var timer = new Timer();
+
+            timer.Elapsed += TimerEventProcessor;
+            timer.Interval = 5000;
+            timer.Start();
+
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q')
+            {
+            }
+        }
+
+        private static void TimerEventProcessor(object myObject, EventArgs myEventArgs)
+        {
+            Console.WriteLine("Time Elapsed...");
+        }
+    }
+}
