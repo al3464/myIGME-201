232a2d639b195ec4b8214bd0c8d7161b933581d2
al3464 <42984740+al3464@users.noreply.github.com>
2021-03-06 23:31:08 -0500

    PE12

---
+++ ArcherLai_PE12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcherLai_PE12Q3
+{
+    public class Myclass
+    {
+        private string myString;
+        
+        
+        public string thisString
+        {
+            set
+            {
+                thisString = value;
+            }
+        }
+
+        public virtual string getString()
+        {
+            return myString;
+        }
+        class MyDerivedClass : Myclass
+        {
+            public override string getString() 
+            {
+                string text = "output from the derived class";
+                return base.getString() + text;
+            }
+        }
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
