4c3fa8bcafad2e9f0c49d4a496d9aa0fd1214afb
al3464 <42984740+al3464@users.noreply.github.com>
2021-03-10 20:55:21 -0500

    PE_14

--- ArcherLai_PE14/Program.cs
+++ ArcherLai_PE14/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArcherLai_PE14
+namespace ArcherLai_PE14Q1
 {
+    public class Cat
+    {
+        public void Mew()
+        {
+            Console.WriteLine("Meowwwwwww-----");
+        }
+
+    }
+
+    public class CatsFood : Cat, IPets
+    {
+        public void Food()
+        {
+            Console.WriteLine("I want fish!!!");
+        }
+    }
+
+    public interface IPets
+    {
+        void Food();
+        void Mew();
+
+       
+    }
     class Program
     {
+        public static void MyMethod(object myObject)
+        {
+            myObject.GetType();
+            Cat myCat = new Cat();
+            myCat.Mew();
+        }
+
         static void Main(string[] args)
         {
+           
         }
     }
 }
