6d47a800b3fdc3841c929f7840dd5991c1734ba5
al3464 <42984740+al3464@users.noreply.github.com>
2021-03-10 22:30:39 -0500

    ArcherLai_PE14

--- ArcherLai_PE14/Program.cs
+++ ArcherLai_PE14/Program.cs
@@ -6,42 +6,64 @@ using System.Threading.Tasks;
 
 namespace ArcherLai_PE14Q1
 {
-    public class Cat
+    interface IPets
     {
-        public void Mew()
-        {
-            Console.WriteLine("Meowwwwwww-----");
-        }
-
-    }
-
-    public class CatsFood : Cat, IPets
-    {
-        public void Food()
-        {
-            Console.WriteLine("I want fish!!!");
-        }
-    }
-
-    public interface IPets
-    {
-        void Food();
-        void Mew();
-
+        void PetsFood();
        
     }
+
+    public class Dog : IPets
+    {
+        public Dog()
+        {
+            
+        }
+        public void PetsFood()
+        {
+            Console.WriteLine("Meat!!!");
+        }
+    }
+
+    public class Cat : IPets
+    {
+ 
+        public void PetsFood()
+        {
+            Console.WriteLine("Fish!!!");
+        }
+      
+    }
+   
     class Program
     {
         public static void MyMethod(object myObject)
         {
-            myObject.GetType();
-            Cat myCat = new Cat();
-            myCat.Mew();
+            var pets = myObject as Cat;
+            if(pets != null)
+            {
+                pets.PetsFood();
+            }
+            else
+            {
+                var pet = myObject as Dog;
+                if(pet != null)
+                {
+                    pet.PetsFood();
+                }
+                else
+                {
+                    Console.WriteLine("Not cat and not dog.");
+                }
+            }
+            
         }
 
         static void Main(string[] args)
         {
-           
+            Cat cats = new Cat();
+            Dog dogs = new Dog();
+            MyMethod(cats);
+            MyMethod(dogs);
         }
     }
 }
