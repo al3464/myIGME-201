8f618da7d61997d1f2ec045da58abba87e136be8
al3464 <42984740+al3464@users.noreply.github.com>
2021-03-17 20:03:04 -0400

    ArcherLai_PE16

--- ArcherLai_PE16/Class1.cs
+++ ArcherLai_PE16/Class1.cs
@@ -8,6 +8,29 @@ namespace ArcherLai_PE16
 {
     public class Class1
     {
+        public abstract class HotDrink
+        {
+            public bool instant;
+            public bool milk;
+            private byte sugar;
+            public string size;
+            public Customer customer;
+           
+            public virtual void AddSugar(byte amount)
+            {
 
+            }
+
+            public HotDrink(string brand)
+            {
+
+            }
+
+        }
+
+        public class Customer
+        {
+
+        }
     }
 }
