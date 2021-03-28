c19fccddce812ea2ff7493885e0a4ced0571a4ef
al3464 <42984740+al3464@users.noreply.github.com>
2021-03-27 22:07:34 -0400

    Unittest2

---
+++ UnitTest2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest2
+{
+    
+        public abstract class GamesPlay
+        {
+            string gameGenre;
+            public abstract void Login();
+        }
+
+        public interface INintendo
+           {
+            void nintendo();
+
+        }
+
+        public interface ISteam
+        {
+            void steam();
+        }
+
+        public class Pokemon : INintendo
+        {
+            public int pixie;
+            public virtual void nintendo()
+            {
+            Console.WriteLine("Welcome to Pokemon.");
+            }
+        }
+
+
+        public class Pikachu : Pokemon
+        { 
+            public override void nintendo()
+            {
+            Console.WriteLine("Pikachu-------");
+            }  
+    
+        }
+
+
+
+        public class Csgo : ISteam
+        {
+            public int guns;
+            public virtual void steam()
+            {
+                
+            }
+            
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             void Call(Pokemon poke) 
+             {
+                poke.nintendo();
+             }
+
+           Pokemon pokemon = new Pokemon();
+           Pikachu pikachu = new Pikachu();
+            pokemon.nintendo();
+            pikachu.nintendo();
+             
+        }
+
+    }
+
+
+    
+}
---
+++ UnitTest2Q4/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest2Q4
+{
+    public interface PhoneInterface {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+    
+    }
+
+    public abstract class Phone {
+        private string phoneNumber;
+
+        public string address;
+
+        public string PhoneNumber;
+        public abstract void Connect();
+        public abstract void Disconnect();
+    
+    }
+
+
+    public class PushButtonPhone : PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public virtual void Connect()
+        {
+
+        }
+
+        public virtual void Disconnect()
+        {
+
+        }
+    }
+
+
+    public class RotaryPhone : PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public virtual void Connect() 
+        { 
+        }
+        
+      
+        public virtual void Disconnect()
+        {
+
+        }
+
+
+    }
+
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho;
+        public string FemaleSideKick;
+        public void TimeTravel()
+        {
+            
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor() 
+        {
+
+        }
+        public void CloseDoor() 
+        {
+            
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+            
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
