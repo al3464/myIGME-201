d7dbb3608ed724a57e76877b490e15372e01f3c5
al3464 <42984740+al3464@users.noreply.github.com>
2021-04-08 23:19:04 -0400

    pe22

--- ArcherLai_PE21 - 副本/Form1.cs
+++ ArcherLai_PE21 - 副本/Form1.cs
@@ -24,17 +24,7 @@ namespace ArcherLai_PE21
             this.cutToolStripMenuItem.Click += new EventHandler(CutToolStripMenuItem__Click);
             this.pasteToolStripMenuItem.Click += new EventHandler(PasteToolStripMenuItem__Click);
 
-            this.boldToolStripMenuItem.Click += new EventHandler(BoldToolStripMenuItem__Click);
-            this.italicsToolStripMenuItem.Click += new EventHandler(ItalicsToolStripMenuItem__Click);
-            this.underlineToolStripMenuItem.Click += new EventHandler(UnderlineToolStripMenuItem__Click);
-
-            this.mSSansSerifToolStripMenuItem.Click += new EventHandler(MSSanSerifToolStripMenuItem__Click);
-            this.timesNewRomanToolStripMenuItem.Click += new EventHandler(TimesNewRomanToolStripMenuItem__Click);
-
             this.toolStrip.ItemClicked += new ToolStripItemClickedEventHandler(ToolStrip__ItemClicked);
-
-            this.richTextBox.SelectionChanged += new EventHandler(RichTextBox__SelectionChanged);
-
             this.Text = "MyEditor";
         }
 
@@ -42,76 +32,6 @@ namespace ArcherLai_PE21
         {
             richTextBox.Clear();
             this.Text = "MyEditor";
-        }
-
-        private void MSSanSerifToolStripMenuItem__Click(object sender, EventArgs e)
-        {
-            Font newFont = new Font("MS Sans Serif", richTextBox.SelectionFont.Size, richTextBox.SelectionFont.Style);
-
-            richTextBox.SelectionFont = newFont;
-        }
-
-        private void TimesNewRomanToolStripMenuItem__Click(object sender, EventArgs e)
-        {
-            Font newFont = new Font("Times New Roman", richTextBox.SelectionFont.Size, richTextBox.SelectionFont.Style);
-
-            richTextBox.SelectionFont = newFont;
-        }
-
-        private void RichTextBox__SelectionChanged(object sender, EventArgs e)
-        {
-            if(this.richTextBox.SelectionFont != null)
-            {
-                this.boldToolStripButton.Checked = richTextBox.SelectionFont.Bold;
-                this.boldToolStripButton.Checked = richTextBox.SelectionFont.Italic;
-                this.underlineToolStripButton.Checked = richTextBox.SelectionFont.Underline;
-            }
-
-            this.colorToolStripButton.BackColor = richTextBox.SelectionColor;
-        }
-
-
-        private void BoldToolStripMenuItem__Click(object sender, EventArgs e)
-        {
-            FontStyle fontStyle = FontStyle.Bold;
-            Font selectionFont = null;
-
-            selectionFont = richTextBox.SelectionFont;
-            if(selectionFont == null)
-            {
-                selectionFont = richTextBox.Font;
-            }
-
-            SetSelectionFont(fontStyle, !selectionFont.Bold);
-
-        }
-
-        private void ItalicsToolStripMenuItem__Click(object sender, EventArgs e)
-        {
-            FontStyle fontStyle = FontStyle.Italic;
-            Font selectionFont = null;
-
-            selectionFont = richTextBox.SelectionFont;
-            if (selectionFont == null)
-            {
-                selectionFont = richTextBox.Font;
-            }
-
-            SetSelectionFont(fontStyle, !selectionFont.Italic);
-        }
-
-        private void UnderlineToolStripMenuItem__Click(object sender, EventArgs e)
-        {
-            FontStyle fontStyle = FontStyle.Underline;
-            Font selectionFont = null;
-
-            selectionFont = richTextBox.SelectionFont;
-            if (selectionFont == null)
-            {
-                selectionFont = richTextBox.Font;
-            }
-
-            SetSelectionFont(fontStyle, !selectionFont.Underline);
         }
 
         private void OpenToolStripMenuItem__Click(object sender, EventArgs e)
@@ -232,9 +152,6 @@ namespace ArcherLai_PE21
             this.richTextBox.SelectionFont = newFont;
         }
 
-        private void bolsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
 
-        }
     }
 }
