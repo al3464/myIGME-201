fde167ebac81a2b8e1b6314c2e92391374615277
al3464 <42984740+al3464@users.noreply.github.com>
2021-04-27 07:11:28 -0400

    123

--- ArcherLai_UnitTest3Q3/Form1.cs
+++ ArcherLai_UnitTest3Q3/Form1.cs
@@ -15,6 +15,49 @@ namespace ArcherLai_UnitTest3Q3
         public Form1()
         {
             InitializeComponent();
+            radioButton1.CheckedChanged += new EventHandler(RadioButton1__CheckedChanged);
+            radioButton2.CheckedChanged += new EventHandler(RadioButton2__CheckedChanged);
+            radioButton3.CheckedChanged += new EventHandler(RadioButton3__CheckedChanged);
+            radioButton4.CheckedChanged += new EventHandler(RadioButton4__CheckedChanged);
+            radioButton5.CheckedChanged += new EventHandler(RadioButton5__CheckedChanged);
+            radioButton6.CheckedChanged += new EventHandler(RadioButton6__CheckedChanged);
+
         }
+        private void RadioButton1__CheckedChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("");
+        }
+
+        private void RadioButton2__CheckedChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.worldometers.info/coronavirus/country/uk/");
+        }
+
+        private void RadioButton3__CheckedChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("");
+        }
+
+        private void RadioButton4__CheckedChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("");
+
+        }
+
+        private void RadioButton5__CheckedChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("");
+        }
+
+        private void RadioButton6__CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
